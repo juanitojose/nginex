860f7f7f19763f3430b6c9c2eb688145dcf5e6da
jjaramib <jjaramib@pichincha.com>
2024-10-16 16:20:35 -0600

    hhh
    hhh

--- tests/test_nginx.py
+++ tests/test_nginx.py
@@ -14,9 +14,9 @@ if not VERSION:
 class TestNginxExTemplate:
 
     def setup_method(self):
-        self.oc_api = OpenShiftAPI(pod_name_prefix="nginx-example")
+        self.oc_api = OpenShiftAPI(pod_name_prefix="nginex")
         json_raw_file = self.oc_api.get_raw_url_for_json(
-            container="nginx-container", dir="imagestreams", filename="nginx-rhel.json"
+            container="nginex", dir="imagestreams", filename="nginx-rhel.json"
         )
         self.oc_api.import_is(path=json_raw_file, name="nginx")
 
@@ -25,13 +25,13 @@ class TestNginxExTemplate:
 
     def test_template_inside_cluster(self):
         template_json = self.oc_api.get_raw_url_for_json(
-            container="nginx-ex", dir="openshift/templates", filename="nginx.json"
+            container="nginex", dir="openshift/templates", filename="nginx.json"
         )
         assert self.oc_api.deploy_template(
             template=template_json, name_in_template="nginx-example", expected_output="Welcome to your static nginx application",
             openshift_args=["SOURCE_REPOSITORY_REF=master", f"NGINX_VERSION={VERSION}", "NAME=nginx-example"]
         )
-        assert self.oc_api.template_deployed(name_in_template="nginx-example")
+        assert self.oc_api.template_deployed(name_in_template="nginex")
         assert self.oc_api.check_response_inside_cluster(
             name_in_template="nginx-example", expected_output="Welcome to your static nginx application"
         )
@@ -41,10 +41,10 @@ class TestNginxExTemplate:
             container="nginx-ex", dir="openshift/templates", filename="nginx.json"
         )
         assert self.oc_api.deploy_template(
-            template=template_json, name_in_template="nginx-example", expected_output="Welcome to your static nginx application",
+            template=template_json, name_in_template="nginex", expected_output="Welcome to your static nginx application",
             openshift_args=["SOURCE_REPOSITORY_REF=master", f"NGINX_VERSION={VERSION}", "NAME=nginx-example"]
         )
-        assert self.oc_api.template_deployed(name_in_template="nginx-example")
+        assert self.oc_api.template_deployed(name_in_template="nginex")
         assert self.oc_api.check_response_outside_cluster(
-            name_in_template="nginx-example", expected_output="Welcome to your static nginx application"
+            name_in_template="nginx-example", expected_output="Welcome to your static nginex application"
         )
